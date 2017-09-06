0d523ca5f88b63c59d103ffba99896099da920cc
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-09-07 04:48:29 +0530

    Create layout files

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,20 @@
-<?php
+<!-- MinuteGen -->
+<!-- An automated web system for board meetings with some automated functionalities -->
+<!-- including tracking matters and generating minutes. -->
 
-?>
+<!-- index.php -->
+<html>
+  <head>
+    <!-- Include main stylesheet -->
+    <link rel="stylesheet" type="text/css" href="./css/main.css">
+  </head
+
+  <?php
+  include_once '../app/views/layouts/header.php';
+  include_once '../app/views/layouts/search_bar.php';
+  include_once '../app/views/layouts/menu.php';
+
+
+  ?>
+
+</html>
