8ebe5a72a2150be9c901daa50ca6ede7bf4cd098
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-09-08 11:21:19 +0530

    Add home view

--- src/public/index.php
+++ src/public/index.php
@@ -1,22 +1,4 @@
-<!-- MinuteGen -->
-<!-- An automated web system for board meetings with some automated functionalities -->
-<!-- including tracking matters and generating minutes. -->
-
-<!-- index.php -->
-<html>
-  <head>
-    <!-- Include main stylesheet -->
-    <link rel="stylesheet" type="text/css" href="./css/main.css">
-    <!-- Include color pallette -->
-    <link rel="stylesheet" type="text/css" href="./css/colors.css">
-  </head
-
-  <?php
-  include_once '../app/views/layouts/header.php';
-  include_once '../app/views/layouts/search_bar.php';
-  include_once '../app/views/layouts/menu.php';
-
-
-  ?>
-
-</html>
+<?php
+    // Load the home page
+    require_once "../app/views/home.php";
+ ?>
