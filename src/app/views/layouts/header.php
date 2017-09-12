f741924dd0b15d67f536bcb715e075ea272fc4b1
chamika <hcph1994@gmail.com>
2017-09-12 07:14:09 +0530

    Add header view

--- src/app/views/layouts/header.php
+++ src/app/views/layouts/header.php
@@ -1,1 +1,59 @@
 <!-- Header of the page -->
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="utf-8">
+    <title>Header view</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Iconscss">
+
+    <style media="screen">
+      topnav{
+        color: white;
+        font-size: 35px;
+        font-style: bold;
+        background-color: #111;
+      }
+    </style>
+
+  </head>
+
+  <body>
+    <div class="top-nav">
+      <div class="row" style="margin-left: 10px;">
+        <div class="col-1">
+          <i class="material-icons" style="font-size:30px;">menu</i>  BOSIUD
+
+        </div>
+        <div class="col2">
+          <input type="text" id="myInput" name="search" placeholder="Search..">
+
+        </div>
+        <div class="col-3">
+          <a href="#" class="fa fa-cog"></a>
+
+        </div>
+        <div class="col-3">
+          <a href="#" class="fa fa-bell-o"></a>
+
+        </div>
+        <div class="col-3">
+          <a href="#" class="fa fa-user"></a>
+
+        </div>
+
+      </div>
+      <!--WHAT IS THIS???-->
+      <div class="menu-bar">
+	        <div class="bar1"></div>
+	        <div class="bar2"></div>
+	        <div class="bar3"></div>
+	    </div>
+
+    </div>
+
+
+  </body>
+</html>
