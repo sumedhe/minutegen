5d5b30257bf0e35a06a8644ebb79f27770a24428
surankalana <surankalanapeiris@gmail.com>
2017-09-23 20:49:28 +0530

    New side nav

--- src/app/controllers/home.php
+++ src/app/controllers/home.php
@@ -8,9 +8,9 @@ class Home extends Controller
   {
     // Generate view
     $this->view('layouts/header');
-    $this->view('pages/sideNav');
     $this->view('layouts/topBar');
     $this->view('layouts/searchBar');
+    $this->view('pages/hovSideNav');
     $this->view('layouts/pages');
     $this->view('pages/minutes');
     $this->view('layouts/footer');
---
+++ src/app/views/pages/hovSideNav.php
@@ -0,0 +1,11 @@
+<div id="mySidenav">
+  <a href="dashboard.php" id="dashboard"> Dashboard <i class="material-icons md-26">home</i>  </a>
+  <!-- Matters link -->
+  <a href="matterAccordion.php" id="matters"> Matters <i class="material-icons md-26">note</i>  </a>
+  <!-- Minutes link -->
+  <a href="minute.php" id="minutes"> Minutes <i class="material-icons md-26">pageview</i>  </a>
+  <!-- Members link -->
+  <a href="member.php" id="members"> Members <i class="material-icons md-26">people</i>  </a>
+    <!-- Help link -->
+  <a href="help.php" id="help"> Help <i class="material-icons md-26">help</i></a>
+</div>
