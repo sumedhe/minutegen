80d76078356c8ede38325b3ce0b310c0bfe91be1
surankalana <surankalanapeiris@gmail.com>
2017-09-26 10:58:38 +0530

    Side nav

--- src/app/views/pages/hovSideNav.php
+++ src/app/views/pages/hovSideNav.php
@@ -1,11 +1,11 @@
-<div id="mySideNav">
-  <a href="dashboard.php" id="dashboard"> Dashboard <i class="material-icons md-26">home</i>  </a>
+<div id="hov-side-nav">
+  <a href="dashboard.php" id="hov-dashboard"> Dashboard <i class="material-icons md-26">home</i>  </a>
   <!-- Matters link -->
-  <a href="matterAccordion.php" id="matters"> Matters <i class="material-icons md-26">note</i>  </a>
+  <a href="matterAccordion.php" id="hov-matters"> Matters <i class="material-icons md-26">note</i>  </a>
   <!-- Minutes link -->
-  <a href="minute.php" id="minutes"> Minutes <i class="material-icons md-26">pageview</i>  </a>
+  <a href="minute.php" id="hov-minutes"> Minutes <i class="material-icons md-26">pageview</i>  </a>
   <!-- Members link -->
-  <a href="member.php" id="members"> Members <i class="material-icons md-26">people</i>  </a>
+  <a href="member.php" id="hov-members"> Members <i class="material-icons md-26">people</i>  </a>
     <!-- Help link -->
-  <a href="help.php" id="help"> Help <i class="material-icons md-26">help</i></a>
+  <a href="help.php" id="hov-help"> Help <i class="material-icons md-26">help</i></a>
 </div>
