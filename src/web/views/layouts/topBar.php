73966226dcfdb20180146f852be248f993b66ce8
steffidevo <steffidevo@gmail.com>
2017-10-06 13:59:08 +0530

    Created the Topbar

--- src/web/views/layouts/topBar.php
+++ src/web/views/layouts/topBar.php
@@ -1,1 +1,22 @@
-<h1> MinuteGen </h1>
+<ul>
+<div class="topnav" style="color: white; font-size: 35px; font-style: bold; background-color: #1B1E24;">
+		<div style="margin-left: 10px">
+		<span style="cursor:pointer" onclick="openNav()" class="menu top"><i class="material-icons menu">menu</i></span>
+    <span href="#" class="logo">MinuteGen</span>
+      <input type="text" id="myInput" name="search" placeholder="Search..">
+			<li><a href="#"><i class="fa fa-cog"></i></a></li>
+			<li><a href="#"><i class="fa fa-user"></i></a> </li>
+			      <li id="noti_Container">
+            <div id="noti_Counter"></div>   <!--SHOW NOTIFICATIONS COUNT.-->
+						<div id="noti_Button"></div>
+						<!--THE NOTIFICAIONS DROPDOWN BOX.-->
+            <div id="notifications">
+                <h3>Notifications</h3>
+                <div style="height:300px;"></div>
+                <div class="seeAll"><a href="#">See All</a></div>
+            </div>
+
+      </div>
+  </div>
+
+</ul>
