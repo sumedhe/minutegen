2dcf0985f44f95128032a11ce1cc3706bb9768fb
samabey1994 <samadhi12abeywickrama@gmail.com>
2017-10-09 11:52:48 +1100

    creating help function

---
+++ src/web/views/pages/help.php
@@ -0,0 +1,120 @@
+<div>
+	<div class="box">
+	  <div class="container-1">
+	  	<div class="box-1">
+	  		<div class="container-2">
+
+
+				  	<h2 class="help">     Welcome to BOSIUD Help Center     </h2>
+
+
+
+				  	<button class="help">Dashboard view</button>
+				<div class="help_panel">
+				  <p>
+				  	<ul>
+	  					<li>Holding card</li>
+	  						<ul> 
+	  							<p>This shows the number of  matters that are not yet completed to a state where it could be added as a matter to a minute</p>
+	  							
+	  						</ul>
+	  					<li>Awaiting card</li>
+	  						<ol>
+	  							<p>####################################################</p>
+
+
+	  						 </ol>
+	  					<li>Pending card</li>
+
+	  						<p>This shows the number of matters that are not either approved or rejected</p>
+					</ul>
+				  </p>
+				</div>
+
+
+				    
+				<button class="help">Search/Add/Edit/Delete Matters</button>
+				<div class="help_panel">
+				  <p>
+				  	<ul>
+	  					<li>Search Matters</li>
+	  						<ul> You can search matters by 
+	  							<li>Matter index (Ex : 129.31.2)</li>
+	  							<li>Minute number (Ex : 129)</li>
+	  							<li>keywords ("accepted " / "rejected")</li>
+	  							<li>any other keyword</li>
+	  						</ul>
+	  					<li>Add Matters </li>
+	  						<ol>
+	  							<li>Click the plus mark at the bottom right coner of the dashboard</li>
+	  							<li>Fill the given form</li>
+	  							<li>Click Save</li>
+
+
+	  						 </ol>
+	  					<li>Edit/Delete Matters</li>
+							<p>The right end of ever matter contains a set of icons which includes Delete,Edit.</p>
+					</ul>
+				  </p>
+				</div>
+
+				<button class="help">Search/Add/Edit/View/Delete minutes</button>
+				<div class="help_panel">
+				  <p>
+				  	<ul>
+	  					<li>Search Minutes</li>
+	  						<ul> You can search matters by
+	  							<li>Minute number (Ex : 129)</li>
+	  							<li>keywords </li>
+	  							<li>date</li>
+	  							<li>state<li>
+	  							
+	  						</ul>
+	  					<li>Add Minutes </li>
+	  						<ol>
+	  							<li>Click the plus mark at the bottom right coner of the dashboard</li>
+	  							<li>Fill the given form</li>
+	  							<li>Click Save</li>
+
+
+	  						 </ol>
+	  					<li>Edit/Delete Matters</li>
+
+	  						<p>The right end of ever matter contains a set of icons which includes Delete,Edit.</p>
+					</ul>
+				  </p>
+				</div>
+
+				<button class="help">Add/Delete/Search members</button>
+				<div class="help_panel">
+				  <p>
+				  	<ul>
+	  					<li>Search Members</li>
+	  						<ul> You can search members by
+	  							<li>Member name</li>
+	  							
+	  							
+	  						</ul>
+	  					<li>Add Members </li>
+	  						<ol>
+	  							<li>Click the plus mark at the bottom right coner of the dashboard</li>
+	  							<li>Fill the given form</li>
+	  							<li>Click Save</li>
+
+
+	  						 </ol>
+	  					<li>Delete Members</li>
+
+	  						<p>#############################################</p>
+					</ul>
+				  </p>
+				</div>
+
+	   
+	      
+	 	 </div>
+			</div> 
+
+	 </div>
+	</div> 
+</div>
