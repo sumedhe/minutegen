7e599de34bde12df53d7429bae05d97cd5147399
steffidevo <steffidevo@gmail.com>
2017-10-09 12:00:30 +0530

    Modified notifying counter

--- src/public/js/notification.js
+++ src/public/js/notification.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
                 // TOGGLE (SHOW OR HIDE) NOTIFICATION WINDOW.
                 $('#notifications').fadeToggle('fast', 'linear', function () {
                     if ($('#notifications').is(':hidden')) {
-                        $('#noti_Button').css('background-color', '#8b8f99');
+                        $('#noti_Button').css('background-color', '#6f97fc');
                     }
                     else $('#noti_Button').css('background-color', '#FFF');        // CHANGE BACKGROUND COLOR OF THE BUTTON.
                 });
@@ -30,7 +30,7 @@ $(document).ready(function () {
                 // CHECK IF NOTIFICATION COUNTER IS HIDDEN.
                 if ($('#noti_Counter').is(':hidden')) {
                     // CHANGE BACKGROUND COLOR OF THE BUTTON.
-                    $('#noti_Button').css('background-color', '#8b8f99');
+                    $('#noti_Button').css('background-color', '#6f97fc');
                 }
             });
 
