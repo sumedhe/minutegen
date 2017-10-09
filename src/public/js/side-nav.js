e0aa2e4276a128055c15626929b21ba78a797204
steffidevo <steffidevo@gmail.com>
2017-10-09 13:32:25 +0530

    Modified topbar  and sidenav

--- src/public/js/side-nav.js
+++ src/public/js/side-nav.js
@@ -1,9 +1,11 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "275px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.7)";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "65px";
+    document.body.style.backgroundColor = "white";
 }
